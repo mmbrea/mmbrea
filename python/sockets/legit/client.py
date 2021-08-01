08cf0563e2b231e808f222fdbe7cc76f3608e983
Dahla <71585108+mmbrea@users.noreply.github.com>
2021-07-31 23:38:20 -0700

    Implement basic client and server

--- python/sockets/legit/client.py
+++ python/sockets/legit/client.py
@@ -1,8 +1,10 @@
 """
-client - Connect to a server and send stdin over the connection.
+client - Connect to a server and send standard input over the connection.
 """
 
 # built-in
+import argparse
+import socket
 import sys
 from typing import List
 
@@ -17,7 +19,19 @@ def main(argv: List[str] = None) -> int:
     if argv is not None:
         command_args = argv
 
-    print(command_args)
-    print(__name__)
+    desc = "Connect to a server and send messages from standard input."
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("host", type=str, help="host to connect to")
+    parser.add_argument("port", type=int, help="port to connect to on host")
+    parsed = parser.parse_args(command_args[1:])
+
+    # connect to the server
+    with socket.create_connection((parsed.host, parsed.port)) as sock:
+        while True:
+            try:
+                data = input("=> ")
+                sock.send(data.encode())
+            except (KeyboardInterrupt, ConnectionAbortedError):
+                break
 
     return result
--- python/sockets/legit/server.py
+++ python/sockets/legit/server.py
@@ -4,6 +4,8 @@ server - Wait for a connection, echo incoming data to stdout, send stdin to
 """
 
 # built-in
+import argparse
+import socket
 import sys
 from typing import List
 
@@ -18,7 +20,31 @@ def main(argv: List[str] = None) -> int:
     if argv is not None:
         command_args = argv
 
-    print(command_args)
-    print(__name__)
+    desc = ("Listen for connections, print incoming messages, "
+            + "send input back to clients.")
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=0,
+        required=False,
+        help="tcp port to listen on",
+    )
+    parsed = parser.parse_args(command_args[1:])
+
+    # create the server socket
+    with socket.create_server(("", parsed.port)) as sock:
+        print(sock.getsockname())
+        client, addr = sock.accept()
+        print("'{}' connected!".format(addr))
+        while True:
+            try:
+                data = client.recv(1024)
+                if not data:
+                    break
+                print(data.decode())
+            except KeyboardInterrupt:
+                break
 
     return result
